003b87ec322d0a2b173851b3de7fba9cd1fd2c7c
Gaolingx <947770192@qq.com>
2024-07-15 00:02:19 +0800

    2024071501

---
+++ 2024c#内功训练/c#高级笔记记录/20240619/ConsoleApp4/ConsoleApp4/RunMain.cs
@@ -0,0 +1,14 @@
+﻿
+public class RunMain()
+{
+    public void TestGetType()
+    {
+        //通过 typeof 运算符获取Type类型
+        Type type01 = typeof(StudentInfo);
+
+        //通过实例对象获取类型
+        StudentInfo studentInfo = new StudentInfo(); //创建对象
+        Type type02 = studentInfo.GetType(); //GetType 是Object 这个类的方法。
+                                             //由于所有类型都继承自Object，所以所有的类型都含有GetType 方法
+    }
+}
---
+++ 2024c#内功训练/c#高级笔记记录/20240619/ConsoleApp4/ConsoleApp4/StudentInfo.cs
@@ -0,0 +1,38 @@
+﻿
+public class StudentInfo
+{
+
+    //无参无返回值的方法
+    public void Run()
+    {
+        Console.WriteLine($"我的名字是{Name}，是米游社的一名创作者");
+    }
+    //有参无返回值方法
+    public void Run2(int age)
+    {
+        Console.WriteLine($"我的名字是{Name}，我今年{age}岁了");
+    }
+    //有参有返回值私有方法
+    public string Run3(string name)
+    {
+        return $"我的名字是{name}，我是一个私有方法";
+    }
+
+    //构造函数
+    public StudentInfo()
+    {
+
+    }
+
+    public StudentInfo(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    private string _studentId; //字段
+    private int _Id { get; set; } //属性（访问器），get set实际上是两个方法
+    public int Age { get; set; }
+    public string Name { get; set; }
+
+}
