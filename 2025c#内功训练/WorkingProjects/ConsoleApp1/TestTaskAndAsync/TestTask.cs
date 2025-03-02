f13c5d65cea3f90c61626f806d6ddaf847b39412
Gaolingx <947770192@qq.com>
2025-03-03 02:38:17 +0800

    2025030301

---
+++ 2025c#内功训练/WorkingProjects/ConsoleApp1/TestTaskAndAsync/TestTask.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System.Runtime.CompilerServices;
+
+namespace TestTaskAndAsync
+{
+    internal class TestTask
+    {
+        [Test]
+        public async Task TestGetThreadId()
+        {
+            Helper.PrintThreadId("Before");
+            await FooAsync();
+            Helper.PrintThreadId("After");
+        }
+
+        async Task FooAsync()
+        {
+            Helper.PrintThreadId("Before");
+            await Task.Delay(1000);
+            Helper.PrintThreadId("After");
+        }
+
+        class Helper
+        {
+            private static int index = 1;
+            public static void PrintThreadId(string? message = null, [CallerMemberName] string? name = null)
+            {
+                var title = $"index: {index}, CallerMemberName: {name}";
+                if (!string.IsNullOrEmpty(message))
+                    title += $" @ {message}";
+                Console.WriteLine($"Thread id: {Environment.CurrentManagedThreadId}, {title}");
+                Interlocked.Increment(ref index);
+            }
+        }
+    }
+}
